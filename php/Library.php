291103d3ec0cc95275383b4375c78ac0c2dbc285
MIbrahim9276 <m.ibrahim9276@gmail.com>
2024-12-26 19:03:54 +0200

    Updated Corresponding PHP

--- php/Library.php
+++ php/Library.php
@@ -14,7 +14,7 @@
     </head>
     <body>
         <!--Main Container-->
-        <div class="container-fluid vh-100" style="margin: 0; padding: 0;">
+        <div class="container-fluid vh-100">
             <!--Navigation Bar (Remove if Unnecessary)-->
             <nav class="navbar navbar-expand bg-body-secondary fixed-top">
                 <div class="container-fluid px-5">
@@ -46,70 +46,68 @@
             <!--Put your code here-->   
             <main class="text-center">
                 <div class="h1 text-primary fw-bolder display-3 mt-5 pt-5 my-4">Library</div>
-                <div class="container text-center d-flex">
-                    <div class="row">
-                        <div class="col d-flex justify-content-center">
-                            <div class="card border mb-3" style="width: 18rem; height: 30rem;">
-                                <img src="https://i.pinimg.com/1200x/ee/75/c2/ee75c2e0e98ed169c1e45a7408492eda.jpg" class="card-img-top" alt="..." style="height: 25rem;">
-                                <div class="card-body">
-                                  <p class=" card-text fw-bolder fs-3">A Hat In Time</p>
-                                </div>
-                              </div>
-                          </div>
-                        <div class="col d-flex justify-content-center">
-                            <div class="card border mb-3" style="width: 18rem; height: 30rem;">
-                                <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEggV3ITapTPdB0XxcFKyzIj0rDg0x3o7LGpQgl1J7GU0JEpQLOuyqihKzkJ9Xg4sySVzPLRA6XAcEbrT4tJKbElbEM2VnkBGV38M35pOma2uoey97HzWggqvYIBQFmnMUzY5yvsPIMeVmQW/s1600/611153-celeste-linux-front-cover.jpg" class="card-img-top" alt="..." style="height: 25rem;">
-                                <div class="card-body">
-                                  <p class="card-text fw-bolder fs-3">Celeste</p>
-                                </div>
-                              </div>
-                          </div>  
+                <div class="container text-center">
+                    <div class="row g-4 row-cols-2 row-cols-lg-4">
                       <div class="col d-flex justify-content-center">
-                        <div class="card border mb-3" style="width: 18rem; height: 30rem;">
-                            <img src="https://i.pinimg.com/1200x/66/58/a3/6658a371128fcaf789204ce9ac7c4f3e.jpg" class="card-img-top" alt="..." style="height: 25rem;">
-                            <div class="card-body">
+                        <div class="card border">
+                            <img src="https://i.pinimg.com/1200x/ee/75/c2/ee75c2e0e98ed169c1e45a7408492eda.jpg" class="card-img-top h-75" alt="...">
+                            <div class="card-body d-flex align-items-center justify-content-center">
+                              <p class=" card-text fw-bolder fs-3">A Hat In Time</p>
+                            </div>
+                          </div>
+                        </div>
+                      <div class="col d-flex justify-content-center">
+                        <div class="card border">
+                          <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEggV3ITapTPdB0XxcFKyzIj0rDg0x3o7LGpQgl1J7GU0JEpQLOuyqihKzkJ9Xg4sySVzPLRA6XAcEbrT4tJKbElbEM2VnkBGV38M35pOma2uoey97HzWggqvYIBQFmnMUzY5yvsPIMeVmQW/s1600/611153-celeste-linux-front-cover.jpg" class="card-img-top h-75" alt="...">
+                          <div class="card-body d-flex align-items-center justify-content-center">
+                            <p class="card-text fw-bolder fs-3">Celeste</p>
+                          </div>
+                        </div>
+                      </div>  
+                      <div class="col d-flex justify-content-center">
+                        <div class="card border">
+                            <img src="https://i.pinimg.com/1200x/66/58/a3/6658a371128fcaf789204ce9ac7c4f3e.jpg" class="card-img-top h-75" alt="...">
+                            <div class="card-body d-flex align-items-center justify-content-center">
                               <p class="card-tex fw-bolder fs-3">Persona 5 Royal</p>
                             </div>
                           </div>
                       </div>
                       <div class="col d-flex justify-content-center">
-                        <div class="card border mb-3" style="width: 18rem; height: 30rem;">
-                            <img src="https://pbs.twimg.com/media/C6kTCwTWcAI4aZ_.jpg" class="card-img-top" alt="..." style="height: 25rem;">
-                            <div class="card-body">
+                        <div class="card border">
+                            <img src="https://pbs.twimg.com/media/C6kTCwTWcAI4aZ_.jpg" class="card-img-top h-75" alt="...">
+                            <div class="card-body d-flex align-items-center justify-content-center">
                               <p class="card-text fw-bolder fs-3">NieR: Automata</p>
                             </div>
                           </div>
                       </div>
-                    </div>
-                    <div class="row">
-                        <div class="col d-flex justify-content-center">
-                            <div class="card border mb-3" style="width: 18rem; height: 30rem;">
-                                <img src="https://i.pinimg.com/1200x/41/37/87/413787102628eda6b8235773ac5fae3c.jpg" class="card-img-top" alt="..." style="height: 25rem;"> 
-                                <div class="card-body">
-                                  <p class="card-text fw-bolder fs-3">Dark Souls 3</p>
-                                </div>
+                      <div class="col d-flex justify-content-center">
+                          <div class="card border">
+                              <img src="https://i.pinimg.com/1200x/41/37/87/413787102628eda6b8235773ac5fae3c.jpg" class="card-img-top h-75" alt="..."> 
+                              <div class="card-body d-flex align-items-center justify-content-center">
+                                <p class="card-text fw-bolder fs-3">Dark Souls 3</p>
                               </div>
-                          </div>
+                            </div>
+                        </div>
                         <div class="col d-flex justify-content-center">
-                            <div class="card border mb-3" style="width: 18rem; height: 30rem;">
-                                <img src="https://i.pinimg.com/1200x/b7/c1/23/b7c1233bb514dc48907a8493427328a7.jpg" class="card-img-top" alt="..." style="height: 25rem;">
-                                <div class="card-body">
+                            <div class="card border">
+                                <img src="https://i.pinimg.com/1200x/b7/c1/23/b7c1233bb514dc48907a8493427328a7.jpg" class="card-img-top h-75" alt="...">
+                                <div class="card-body d-flex align-items-center justify-content-center">
                                   <p class="card-title fw-bolder fs-3">Hotline Miami 2</p>
                                 </div>
                               </div>
                           </div>  
-                      <div class="col d-flex justify-content-center">
-                        <div class="card border mb-3" style="width: 18rem; height: 30rem;">
-                            <img src="https://i.pinimg.com/1200x/36/3c/a3/363ca37645320f1a058edde4fa133d89.jpg" class="card-img-top" alt="..." style="height: 25rem;">
-                            <div class="card-body">
-                              <p class="card-text fw-bolder fs-3">Eldn Ring</p>
+                        <div class="col d-flex justify-content-center">
+                          <div class="card border">
+                              <img src="https://i.pinimg.com/1200x/36/3c/a3/363ca37645320f1a058edde4fa133d89.jpg" class="card-img-top h-75" alt="...">
+                              <div class="card-body d-flex align-items-center justify-content-center">
+                                <p class="card-text fw-bolder fs-3">Eldn Ring</p>
+                              </div>
                             </div>
-                          </div>
-                      </div>
+                        </div>
                       <div class="col d-flex justify-content-center">
-                        <div class="card border mb-3" style="width: 18rem; height: 30rem;">
-                            <img src="https://i.pinimg.com/1200x/a3/b8/68/a3b868d84f814fd917d5faea6e76f4ff.jpg" class="card-img-top" alt="..." style="height: 25rem;">
-                            <div class="card-body">
+                        <div class="card border">
+                            <img src="https://i.pinimg.com/1200x/a3/b8/68/a3b868d84f814fd917d5faea6e76f4ff.jpg" class="card-img-top h-75" alt="...">
+                            <div class="card-body d-flex align-items-center justify-content-center">
                               <p class="card-text fw-bolder fs-3">Kingdom Hearts 2</p>
                             </div>
                           </div>
--- php/Search_result.php
+++ php/Search_result.php
@@ -14,7 +14,7 @@
     </head>
     <body>
         <!--Main Container-->
-        <div class="container-fluid vh-100" style="margin: 0; padding: 0;">
+        <div class="container-fluid vh-100">
             <!--Navigation Bar (Remove if Unnecessary)-->
             <nav class="navbar navbar-expand bg-body-secondary fixed-top">
                 <div class="container-fluid px-5">
@@ -44,76 +44,48 @@
             </nav>
 
             <!--Put your code here-->
-            <main>
-                <div class="h1 text-primary display-3 fw-bolder ms-5 mt-3 pt-5 my-4">The Results</div>
-                <div class="contaner d-flex ms-5 justify-content-center">
-                    <div class="card mb-3 border" style="max-width: 1500px; height: 250px;">
-                        <div class="row g-0">
-                          <div class="col-md-2">
-                            <img src="https://i.pinimg.com/1200x/2c/df/87/2cdf870b2971003ae764e341704d8165.jpg" class="img-fluid rounded-start" style="width: 220px; height: 245px;">
-                          </div>
-                          <div class="col-md-10">
-                            <div class="card-body">
-                              <h5 class="card-title fs-1">Hotline Miami</h5>
-                              <h5 class="card-text fs-4">60$</h5>
-                              <a href="#" class="btn btn-primary">Add to Cart</a>
-                              <p class="card-text mt-3">Hotline Miami is a top-down shooter game. It is set in Miami during the 1980s,and is divided into nineteen "chapters".At the beginning of each chapter, the player character "Jacket"receives a message on his answering machine, instructing him to travel to a different part of Miami and kill all enemies at that location.</p>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                <div class="contaner d-flex ms-5 justify-content-center">
-                    <div class="card mb-3 border" style="max-width: 1500px; height: 250px;">
-                        <div class="row g-0">
-                          <div class="col-md-2">
-                            <img src="https://i.pinimg.com/1200x/c9/18/3a/c9183af6cfbfa5861839f008a533daba.jpg" class="img-fluid rounded-start" style="width: 220px; height: 245px;">
-                          </div>
-                          <div class="col-md-10">
-                            <div class="card-body">
-                              <h5 class="card-title fs-1">Hollow Knight</h5>
-                              <h5 class="card-text fs-4">60$</h5>
-                              <a href="#" class="btn btn-primary">Add to Cart</a>
-                              <p class="card-text mt-3">Hollow Knight is a 2D side-scrolling Metroidvania.The player controls a silent insectoid protagonist called "the Knight" who explores an underground fallen kingdom called Hallownest. The Knight can strike enemies with a sword-like weapon called a Nail and can learn spells that allow for long-range attacks.</p>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                <div class="contaner d-flex ms-5 justify-content-center">
-                    <div class="card mb-3 border" style="max-width: 1500px; height: 250px;">
-                        <div class="row g-0">
-                          <div class="col-md-2">
-                            <img src="https://i.pinimg.com/1200x/bd/b8/ec/bdb8eca07de123c1c9f6e0801c817e6a.jpg" class="img-fluid rounded-start" style="width: 220px; height: 245px;">
-                          </div>
-                          <div class="col-md-10">
-                            <div class="card-body">
-                              <h5 class="card-title fs-1">Hades</h5>
-                              <h5 class="card-text fs-4">60$</h5>
-                              <a href="#" class="btn btn-primary">Add to Cart</a>
-                              <p class="card-text mt-3">Hollow Knight is a 2D side-scrolling Metroidvania.The player controls a silent insectoid protagonist called "the Knight" who explores an underground fallen kingdom called Hallownest. The Knight can strike enemies with a sword-like weapon called a Nail and can learn spells that allow for long-range attacks.</p>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                <div class="contaner d-flex ms-5 justify-content-center">
-                <div class="card mb-3 border" style="max-width: 1500px; height: 250px;">
-                    <div class="row g-0">
-                      <div class="col-md-2">
-                        <img src="https://i.pinimg.com/1200x/86/a5/cc/86a5ccad082028c2c568ff2db5d213cc.jpg" class="img-fluid rounded-start" style="width: 220px; height: 245px;">
-                      </div>
-                      <div class="col-md-10">
-                        <div class="card-body">
-                          <h5 class="card-title fs-1">Horizon Zero Dawn</h5>
-                          <h5 class="card-text fs-4">60$</h5>
-                          <a href="#" class="btn btn-primary">Add to Cart</a>
-                          <p class="card-text mt-3">Horizon Zero Dawn is an action role-playing game played from a third-person view.Players take control of Aloy, a hunter who ventures through a post-apocalyptic land ruled by robotic creatures.Aloy can kill enemies in a variety of ways shooting them with arrows,setting traps such as tripwires using the Tripcaster,using explosives,and a spear.</p>
-                        </div>
-                      </div>
-                    </div>
+            <main class="container">
+                <div class="h1 text-primary display-3 fw-bolder mt-3 pt-5 my-4">The Results</div>
+
+                <div class="border rounded d-lg-flex mb-4">
+                  <img src="https://i.pinimg.com/1200x/2c/df/87/2cdf870b2971003ae764e341704d8165.jpg" class="col-lg-2 col-12 rounded-start">
+                  <div class="card-body col-10 p-4">
+                    <h5 class="card-title fs-1">Hotline Miami</h5>
+                    <h5 class="card-text fs-4">60$</h5>
+                    <a href="#" class="btn btn-primary">Add to Cart</a>
+                    <p class="card-text mt-3">Hotline Miami is a top-down shooter game. It is set in Miami during the 1980s,and is divided into nineteen "chapters".At the beginning of each chapter, the player character "Jacket"receives a message on his answering machine, instructing him to travel to a different part of Miami and kill all enemies at that location.</p>
                   </div>
-                </div>  
+                </div>
+                
+                <div class="border rounded d-lg-flex mb-4">
+                  <img src="https://i.pinimg.com/1200x/c9/18/3a/c9183af6cfbfa5861839f008a533daba.jpg" class="col-lg-2 col-12 rounded-start">
+                  <div class="card-body col-10 p-4">
+                    <h5 class="card-title fs-1">Hollow Knight</h5>
+                    <h5 class="card-text fs-4">60$</h5>
+                    <a href="#" class="btn btn-primary">Add to Cart</a>
+                    <p class="card-text mt-3">Hollow Knight is a 2D side-scrolling Metroidvania.The player controls a silent insectoid protagonist called "the Knight" who explores an underground fallen kingdom called Hallownest. The Knight can strike enemies with a sword-like weapon called a Nail and can learn spells that allow for long-range attacks.</p>
+                  </div>
+                </div>
+                
+                <div class="border rounded d-lg-flex mb-4">
+                  <img src="https://i.pinimg.com/1200x/bd/b8/ec/bdb8eca07de123c1c9f6e0801c817e6a.jpg" class="col-lg-2 col-12 rounded-start">
+                  <div class="card-body col-10 p-4">
+                    <h5 class="card-title fs-1">Hades</h5>
+                    <h5 class="card-text fs-4">60$</h5>
+                    <a href="#" class="btn btn-primary">Add to Cart</a>
+                    <p class="card-text mt-3">Hollow Knight is a 2D side-scrolling Metroidvania.The player controls a silent insectoid protagonist called "the Knight" who explores an underground fallen kingdom called Hallownest. The Knight can strike enemies with a sword-like weapon called a Nail and can learn spells that allow for long-range attacks.</p>
+                  </div>
+                </div>
+
+                <div class="border rounded d-lg-flex mb-4">
+                  <img src="https://i.pinimg.com/1200x/86/a5/cc/86a5ccad082028c2c568ff2db5d213cc.jpg" class="col-lg-2 col-12 rounded-start">
+                  <div class="card-body col-10 p-4">
+                    <h5 class="card-title fs-1">Horizon Zero Dawn</h5>
+                    <h5 class="card-text fs-4">60$</h5>
+                    <a href="#" class="btn btn-primary">Add to Cart</a>
+                    <p class="card-text mt-3">Hollow Knight is a 2D side-scrolling Metroidvania.The player controls a silent insectoid protagonist called "the Knight" who explores an underground fallen kingdom called Hallownest. The Knight can strike enemies with a sword-like weapon called a Nail and can learn spells that allow for long-range attacks.</p>
+                  </div>
+                </div>
             </main>
         </div>
 
