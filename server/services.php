c747ab4ab3c0b7b7b945c25c432f6fff86503c6d
MIbrahim9276 <m.ibrahim9276@gmail.com>
2024-12-27 17:18:27 +0200

    Added Revised PHP Config

--- server/services.php
+++ server/services.php
@@ -2,7 +2,11 @@
 require_once("./database_access.php");
 
 class UserService {
+    public static $dba;
     public static function register() {
         $dba = new DatabaseAccess();
     }
 }
+
+UserService::register();
+UserService::$dba->close();
