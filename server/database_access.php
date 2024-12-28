d1869b801fed1fcf7f67c6aa1b17ef8a1f208fcb
MIbrahim9276 <m.ibrahim9276@gmail.com>
2024-12-28 13:53:43 +0200

    Created Complete Database and User Backend Service

--- server/database_access.php
+++ server/database_access.php
@@ -2,19 +2,29 @@
 $config = include("./config.local.php");
 
 class DatabaseAccess {
-    private $conn;
+    private mysqli $conn;
+    private mysqli_stmt $stmt;
+    private mysqli_result $result;
 
     public function __construct() {
         global $config;
 
-        $conn = new mysqli($config["DB_HOST"], $config["DB_USER"], $config["DB_PASSWORD"], $config["DB_NAME"]);
-        if ($conn->connect_error) {
+        $this->conn = new mysqli($config["DB_HOST"], $config["DB_USER"], $config["DB_PASSWORD"], "test");
+        if ($this->conn->connect_error) {
             die("Invalid Access to the Database");
         }
     }
-    public function execute($sql, $types, ...$inputs) {
-        $stmt = $this->conn->prepare($sql);
-        $stmt->execute($types, $inputs);
+    public function update($sql, $types = null, ...$inputs) {
+        $this->stmt = $this->conn->prepare($sql);
+        $this->stmt->bind_param($types, ...$inputs);
+        $this->stmt->execute();
+    }
+    public function query($sql, $types = null, ...$inputs) {
+        $this->stmt = $this->conn->prepare($sql);
+        $this->stmt->bind_param($types, ...$inputs);
+        $this->stmt->execute();
+        $this->result = $this->stmt->get_result();
+        return $this->result->fetch_all(MYSQLI_BOTH);
     }
     public function close() {
         $this->conn->close();
---
+++ server/user_service.php
@@ -0,0 +1,54 @@
+<?php
+require_once("./database_access.php");
+
+class UserService {
+    public static function register($details) {
+        $dba = new DatabaseAccess();
+        
+        $dba->update(
+            "INSERT INTO users (username, email, password, is_publisher) VALUES (?, ?, ?, ?)",
+            "sssi",
+            $details["username"], $details["email"], $details["password"], intval(false)
+        );
+
+        $dba->close();
+    }
+    public static function login($credentials) {
+        $dba = new DatabaseAccess();
+
+        $result = $dba->query(
+            "SELECT id FROM users WHERE username=? AND password=?",
+            "ss",
+            $credentials["username"], $credentials["password"]
+        )[0];
+
+        $_SESSION["user_id"] = $result["id"];
+
+        $dba->close();
+    }
+    public static function updateInfo($details) {
+        $dba = new DatabaseAccess();
+
+        $dba->update(
+            "UPDATE users SET username=?, email=? WHERE id=?",
+            "ssi",
+            $details["username"], $details["email"], $_SESSION["user_id"]
+        );
+
+        $dba->close();
+    }
+    public static function changePassword($passwordInfo) {
+        $dba = new DatabaseAccess();
+
+        $dba->update(
+            "UPDATE users SET password=? WHERE id=? AND password=?",
+            "sis",
+            $passwordInfo["newPass"], $_SESSION["user_id"], $passwordInfo["currentPass"]
+        );
+
+        $dba->close();
+    }
+    public static function logout() {
+        unset($_SESSION["user_id"]);
+    }
+}
