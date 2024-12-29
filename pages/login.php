72b2220ac5c05744ca76cb4fa908f8ef027ba799
MIbrahim9276 <m.ibrahim9276@gmail.com>
2024-12-29 15:47:46 +0200

    Implemented Login

--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,19 @@
+<?php
+require_once("../server/user_service.php");
+
+$success = true;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $success = UserService::login([
+    'username' => $_POST["username"],
+    'password' => $_POST["password"]
+  ]);
+
+  if ($success) header("Location: homepage.php");
+}
+
+echo $succss;
+?>
+
 <!DOCTYPE html>
 
 <html>
@@ -21,21 +37,19 @@
                     <div class="card w-50 shadow-sm bg-body-secondary">
                       <div class="card-body bg-body-secondary">
                         <h2 class="text-center mb-4">Log In</h2>
-                        <form>
-                            <!-- Username -->
-                            <div class="mb-3">
-                                <label for="Username" class="form-label">Username</label>
-                                <input type="text" class="form-control" id="Username" name="Username" placeholder="Enter your Username" required>
-                            </div>
+                        <?php
+                        if (!$success) echo "<span class='text-danger'>Username or Password is Invalid</span>";
+                        ?>
+                        <form action="login.php" method="post">
+                          <!-- Username -->
+                          <div class="mb-3">
+                              <label for="username" class="form-label">Username</label>
+                              <input type="text" class="form-control" id="username" name="username" placeholder="Enter your Username" required>
+                          </div>
                           <!-- Password -->
                           <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
                             <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password" required>
-                          </div>
-                          <!-- Remember Me Checkbox -->
-                          <div class="mb-3 form-check">
-                            <input type="checkbox" class="form-check-input" id="rememberMe">
-                            <label class="form-check-label" for="rememberMe">Remember Me</label>
                           </div>
                           <!-- Log In Button -->
                           <div class="d-grid">
@@ -54,5 +68,7 @@
 
         <!--Bootstrap Javascript-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <!--Custom Javascript-->
+        <script src="../assets/js/login.js"></script>
     </body>
 </html>
