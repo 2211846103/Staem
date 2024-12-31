192567c4dc0aa013ce6e676cc014fa5ae11c46eb
MIbrahim9276 <m.ibrahim9276@gmail.com>
2024-12-31 14:58:26 +0200

    Added Missing JQuery CDN

--- pages/login.php
+++ pages/login.php
@@ -77,6 +77,8 @@ echo $succss;
 
         <!--Bootstrap Javascript-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <!--JQuery CDN-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <!--Custom Javascript-->
         <script src="../assets/js/login.js"></script>
     </body>
