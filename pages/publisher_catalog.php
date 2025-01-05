bae7b3e5a6839907e0a08843f6f0664c22017c91
2211849800 <sohaibkshir01@gmail.com>
2025-01-05 23:49:41 +0200

    publisher_catalog

--- pages/publisher_catalog.php
+++ pages/publisher_catalog.php
@@ -1,3 +1,20 @@
+<?php
+
+ require_once("../server/user_service.php");
+require_once("../server/game_service.php");
+require_once("../server/review_service.php");
+require_once("../server/order_service.php");
+
+
+$games = GameService::getGamesbypublisher(publisherId: UserService::isLoggedIn());
+ 
+
+
+
+
+
+
+?>
 <!DOCTYPE html>
 
 <html>
@@ -28,13 +45,24 @@
             </nav>
 
             <!--Main Section-->
-            <main class="d-flex h-100">
+            <?php 
+            $overflow = "overflow-scroll";
+            if (empty($games)) $overflow = "overflow-hidden";
+            echo '
+            <main class="d-flex h-100 '.$overflow.'">'
+              ?>
                 <section class="w-25 h-100 d-flex flex-column container w-25 border-end border-3 p-0">
                   <h3 class="display-6 p-3 bg-primary text-black">My Catalog</h3>
                   <div class="list-group list-group-flush h-100 overflow-scroll">
-                    <button class="list-group-item list-group-item-action">Game 1</button>
-                    <button class="list-group-item list-group-item-action">Game 2</button>
-                    <button class="list-group-item list-group-item-action">Game 3</button>
+                    <?php 
+                    foreach ($games as $game) {
+                      echo '<button class="list-group-item list-group-item-action">'.$game["title"].'</but
+
+                    
+                    <button class="list-group-item list-group-item-action">'.$game["title"].'</button>
+                    <button class="list-group-item list-group-item-action">'.$game["title"].'</button>
+                    <button class="list-group-item list-group-item-action">'.$game["title"].'</button> ';}
+                    ?>
                   </div>
                 </section>
 
@@ -43,56 +71,74 @@
                     <h1 class="display-5">Game Title</h1>
                     <div class="d-flex h-25">
                       <div class="h-100 w-75">
+                        <?php 
+                        echo '
                         <h5>Description</h5>
-                        <p class="text-body-secondary text-truncate">
-                          Sed imperdiet nunc at ligula ultrices viverra.
-                          Donec aliquam id arcu vitae consectetur. Donec rutrum nisi ut nisl consectetur,
-                          hendrerit interdum dolor efficitur. Pellentesque sollicitudin,
-                          lectus eget porttitor tempor, metus nisi semper sem, id elementum tortor urna ac sem. 
-                          Proin vel auctor nulla, et ullamcorper tellus. Phasellus porta tellus quis vulputate maximus.
-                          Duis lobortis massa in mi molestie porta.
+                        <p class="text-body-secondary text-truncate ">
+                          '.$game["description"].'
                         </p>
+                        ';
+                        ?>
                       </div>
                       <div class="p-3 w-25 d-flex flex-column align-items-start justify-content-center">
+                        <?php 
+                        echo'
                         <div class="w-100 d-flex">
                           <span class="fw-bold me-auto">Avg Ratings:</span>
-                          <span> 5.3 <i class="fa-solid fa-star text-info"></i></span>
+                          <span> '.$game["rating"].' <i class="fa-solid fa-star text-info"></i></span>
                         </div>
                         <div class="w-100 d-flex">
                           <span class="fw-bold me-auto">Price:</span>
-                          <span>20.99$</span>
+                          <span>'.$game["price"].'</span>
                         </div>
+                        ';
+                        ?>
                       </div>
                     </div>
                   </div>
                   <div id="game-statistics-container" class="overflow-scroll border-top py-4 px-5">
+                    
                     <h4 class="display-6">Report:</h4>
                     <div class="w-100 d-flex mb-3 justify-content-around">
                       <div class="card h-100 m-3 border bg-body-secondary">
                         <div class="card-body">
+                          <?php
+                          echo'
                           <h4 class="card-title">Total Purchases:</h4>
-                          <h1 class="card-text text-primary">5.3 Million</h1>
+                          <h1 class="card-text text-primary">'.$stats["copies_sold"].'</h1>
                           <h5 class="card-subtitle text-body-secondary">Copies were sold</h5>
+                          ';
+                          ?>
                         </div>
                       </div>
                       <div class="card h-100 m-3 border bg-body-secondary">
                         <div class="card-body">
+                          <?php
+                          echo'
                           <h4 class="card-title">Total Refunds:</h4>
-                          <h1 class="card-text text-primary">3.2 Thousand</h1>
+                          <h1 class="card-text text-primary">'.$stats["refunds"].'</h1>
                           <h5 class="card-subtitle text-body-secondary">People have Refunded</h5>
+                          ';
+                          ?>
                         </div>
                       </div>
                       <div class="card h-100 m-3 border bg-body-secondary">
                         <div class="card-body">
+                          <?php
+                          echo'
                           <h4 class="card-title">Net Revenue:</h4>
-                          <h1 class="card-text text-primary">12.4 Million $</h1>
+                          <h1 class="card-text text-primary">'.$stats["revenue_gained"].'</h1>
                           <h5 class="card-subtitle text-body-secondary">In net revenue</h5>
+                          ';
+                          ?>
                         </div>
                       </div>
                     </div>
                     <h4 class="display-6">Reviews:</h4>
                     <div class="card container h-100 border overflow-scroll">
+
                       <div class="card-body">
+
                         <div class="card border mb-3">
                           <div class="card-header">Author:</div>
                           <div class="card-body d-flex align-items-center">
@@ -104,6 +150,8 @@
                           </div>
                         </div>
                         <div class="card border  mb-3">
+                          <?php
+                          echo'
                           <div class="card-header">Author:</div>
                           <div class="card-body d-flex align-items-center">
                             <div class="card-text me-auto w-75">asdfh asjdks jskdjsj ksjdk jsj ks</div>
@@ -112,6 +160,8 @@
                               <span> 5.3 <i class="fa-solid fa-star text-info"></i></span>
                             </div>
                           </div>
+                          ';
+                          ?>
                         </div>
                         <div class="card border mb-3">
                           <div class="card-header">Author:</div>
@@ -139,6 +189,7 @@
                 </section>
             </main>
         </div>
+        
 
         <!--Bootstrap and Fontawesome Javascript-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
